cc28a228b534d720fdc1bce465b77954105a8c2a
manhaang <mannacubpom@gmail.com>
2023-06-15 23:54:51 +0700

    jsinfo

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+function chengcolor(){
+    
+    // alert("change Color Funtion...")
+    let tcolor= document.querySelector('#in1').value
+    let bcolor= document.querySelector('#in2').value
+    // alert(tcolor)
+    // alert(bcolor)
+    document.querySelector('h2').style.color = tcolor
+    document.querySelector('h2').style.backgroundColor = bcolor
+    console.log('sakonman')
+     }
